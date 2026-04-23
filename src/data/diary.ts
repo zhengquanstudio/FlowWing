78c03f8c8d510b4d6b397d06fcf954172e565ec0
zhengquanstudio <laisy2089@163.com>
2026-04-23 23:01:21 +0800

    chore(content): sync main@d3d04d4

--- src/data/diary.ts
+++ src/data/diary.ts
@@ -13,13 +13,40 @@ export interface DiaryItem {
 
 // 示例日记数据
 const diaryData: DiaryItem[] = [
+
+	{
+		id: 4,
+		content:
+			"日记第四篇，记录美好生活",
+		date: "2026-04-23 T10:30:00Z",
+		images: ["/images/diary/4.png"],
+	},
+
+	{
+		id: 3,
+		content:
+			"日记第三篇，记录美好生活",
+		date: "2026-04-22 T10:30:00Z",
+		images: ["/images/diary/3.png"],
+	},
+	
+	
+	{
+		id: 2,
+		content:
+			"日记第二篇，记录美好生活",
+		date: "2026-04-20 T10:30:00Z",
+		images: ["/images/diary/2.png"],
+	},
+	
 	{
 		id: 1,
 		content:
-			"The falling speed of cherry blossoms is five centimeters per second!",
+			"日记第一篇，记录美好生活",
 		date: "2025-01-15T10:30:00Z",
-		images: ["/images/diary/sakura.jpg", "/images/diary/1.webp"],
-	},
+		images: ["/images/diary/1.png"],
+	},	
+	
 ];
 
 // 获取日记列表（按时间倒序）
